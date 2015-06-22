59da04fa700195648063cd8d7e1d157606df2068
rossacheson <ross.acheson@gmail.com>
2015-06-22 17:27:38 -0400

    //generate rooms (incomplete)

--- HouseTest.java
+++ HouseTest.java
@@ -27,5 +27,17 @@ public class HouseTest {
 		String result = h.getCurrentRoomInfo();
 		assertTrue(result.contains("error")); //there should be an error message
 	}
+	
+	//generateRooms()
+	@Test
+	public void testGenerateRooms() {
+		Room r = mock(Room.class);
+		Room[] rooms = new Room[1];
+		rooms[0] = r;
+		House h = new House(rooms); //real house with fake rooms
+		Room [] result = h.generateRooms(3);
+		//assertArrayEquals(result, result);
+		assertThat(result[2]);
+	}
 
 }
